6d08e192a1db682edffd5db2defbb82feab26276
Rayan <ravenSec93@gmail.com>
2023-02-05 15:25:21 +0100

    05 Feb

--- scanner.py
+++ scanner.py
@@ -2,3 +2,11 @@
 #  pip install python-nmap
 
 import nmap
+
+scanner = nmap.PortScanner()
+
+print("Welcome, this is a simple nmap automation tool")
+print("<----------------------------------------------------")
+
+# Don't copy the code while following the video,
+# follow the video then write the code while trying to understand what's going on !
