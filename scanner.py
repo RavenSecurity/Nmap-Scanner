26d1bec4801a5051ea20c1e8d039f4d80e53677b
Rayan <ravenSec93@gmail.com>
2023-02-07 20:50:42 +0100

    response init

--- scanner.py
+++ scanner.py
@@ -8,5 +8,17 @@ scanner = nmap.PortScanner()
 print("Welcome, this is a simple nmap automation tool")
 print("<----------------------------------------------------")
 
-# Don't copy the code while following the video,
-# follow the video then write the code while trying to understand what's going on !
+ip_addr = input("Please enter the IP address you want to scan: ")
+print("The IP you entered is: ", ip_addr)
+type(ip_addr)
+
+resp = input("""\nPlease enter the type of scan you want to run
+                1)SYN ACK Scan
+                2)UDP Scan
+                3)Comprehensive Scan""")
+print("You have selected option: ", resp)
+
+if resp == '1':
+    print("Nmap  Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, '1-1024', '-v -sS')
+    print(scanner.scaninfo())
