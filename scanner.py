9aace0cfe59106ffbe86e1515872de6a861a373f
Rayan <ravenSec93@gmail.com>
2023-02-09 22:52:15 +0100

    Improved string outputs

--- scanner.py
+++ scanner.py
@@ -10,26 +10,17 @@ print("""
  /    \ /     \\__  \ \____ \ 
 |   |  \  Y Y  \/ __ \|  |_> >
 |___|  /__|_|  (____  /   __/ 
-     \/      \/     \/|__|     """)
-
-print("\n")
-
-
-print("<----------------------------------------------------")
-print("Welcome to Nmap", scanner.nmap_version(), "cli tool")
-print("<----------------------------------------------------")
+     \/      \/     \/|__|     """ , scanner.nmap_version())
 
 print("\n")
 
 ip_addr = input("Please enter the IP address you want to scan: ")
-print("The IP you entered is: ", ip_addr)
 type(ip_addr)
 
 resp = input("""\nPlease enter the type of scan you want to run
                 1)SYN ACK Scan
                 2)UDP Scan
                 3)Comprehensive Scan \n""")
-print("You have selected option: ", resp)
 
 print("\n")
 
@@ -38,7 +29,6 @@ if resp == '1':
     scanner.scan(ip_addr, '1-1024', '-v -sS')
     print(scanner.scaninfo())
     print("Ip Status: ", scanner[ip_addr].state())
-    print(scanner[ip_addr].all_protocols())
     print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
 elif resp == '2' :
     scanner.scan(ip_addr, '1-1024', '-v -sU')
